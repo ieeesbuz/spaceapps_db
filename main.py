3fa2625fd75ba742139bb3755e4c785c4a089445
Arturo H <keyblademaster_01@hotmail.com>
2020-10-03 17:30:50 +0200

    Nothing special

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
+import rasterio
+from rasterio.plot import show
 
 
-url = 'https://daac.ornl.gov/daacdata/cms/CMS_DARTE_V2/data/onroad_2017.tif'
-r = requests.get(url, allow_redirects=True)
-open('onroad_2017.tif', 'wb').write(r.content)
+
+# url = 'https://daac.ornl.gov/daacdata/cms/CMS_DARTE_V2/data/onroad_2017.tif'
+# r = requests.get(url, allow_redirects=True)
+# open('onroad_2017.tif', 'wb').write(r.content)
+
+img = rasterio.open('onroad_2017.tif')
+show(img)
