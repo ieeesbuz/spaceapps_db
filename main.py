e4d700364db99ce018f9f02c389f93f6a35edb17
Arturo H <keyblademaster_01@hotmail.com>
2020-10-04 15:16:08 +0200

    Working

--- main.py
+++ main.py
@@ -8,44 +8,34 @@ import os
 import re
 
 #This function calculates CO2 from raster data
-def CO2_calc (lat, lon, area, band1):
-    ciudad = np.array([lat, lon, area]) #longitud, latitud, area(km^2)
+def CO2_calc (row, col, area, band1):
+    ciudad = np.array([row, col, area]) #longitud, latitud, area(km^2)
 
-    #raster data has ncols 5070 nrows 3560
-    dx = 0.014833485247         #degree increment
-    dy = 0.009892490399
     dx_km = 1.649               #haversine calc for meter increment
     dy_km= 1.100 
 
-    #offset from table origin in degrees
-    lat_add = abs(53.3863-(ciudad[0]))
-    lon_add = abs((ciudad[1])+137.257)
-
-    #translate from degrees to position in table
-    pos_lon = int(round(lon_add/dx))
-    pos_lat = int(round(lat_add/dy))
-
+    
     #calculate how many cells to search, asumes city is square
     lon_mitad = int(round(((math.sqrt(ciudad[2])/2)/dx_km)))
     lat_mitad = int(round(((math.sqrt(ciudad[2])/2)/dy_km)))
 
     #in case cities are small
     if lon_mitad == 0:        
-       c = pos_lon
-       d = pos_lon + 1
+       c = row
+       d = row + 1
     else:
-        c = pos_lon-lon_mitad
-        d = pos_lon+lon_mitad
+        c = row-lon_mitad
+        d = row+lon_mitad
 
     if lat_mitad == 0:
-        a = pos_lat
-        b = pos_lat + 1       
+        a = col
+        b = col + 1       
     else:
-        a = pos_lat-lat_mitad
-        b = pos_lat+lat_mitad
+        a = col-lat_mitad
+        b = col+lat_mitad
 
     #cropped raster CO2 measures
-    submatrix = band1[a:b,c:d]
+    submatrix = band1[c:d,a:b]
 
     #finally we calc the sum
     CO2_sum = np.sum(submatrix)
@@ -81,14 +71,15 @@ with open('uscities.csv', mode='r') as csv_file:
         year = int(m.group(1))
         print(year)
         band1 = dataset.read(1)
-        #print(band1[(2790-11):(2790+11),(3846-16):(3846+16)])
+        #print(band1[(1749-11):(1749+11),(4188-16):(4188+16)])
 
         #calc for every city
         for row in csv_reader:   
             if float(row["density"]) > 0 and coordinatesValid(row) :
                 population = float(row["population"])/(1.037*(2018-year))
                 area = population/float(row["density"])     
-                co2=CO2_calc(float(row["lat"]),float(row["lng"]),area,band1)
+                rows, cols = dataset.index(float(row["lng"]),float(row["lat"]))
+                co2=CO2_calc(rows,cols,area,band1)
                 ratio = co2/population
                 # if ratio == 0:
                 #      print(row)
