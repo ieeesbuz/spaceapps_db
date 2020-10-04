f2442a542cd0c5fc091f5ff5aff82955375653ee
Javier Prados <javierpradosperez@gmail.com>
2020-10-04 13:20:50 +0200

    fixed vulcan projection

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import psycopg2
 import numpy as np
 import math
 import sys
+import os
+import re
+import time
 
 def CO2_calc (lat, lon, area, band1):
     ciudad = np.array([lat, lon, area]) #longitud, latitud, area(km^2)
-
-    dx = 0.014833485247         #ncols  5070 nrows   3560
+    print(ciudad)
+    dx = 0.014833485247         #ncols 5070 nrows 3560
     dy = 0.009892490399
     dx_km = 1.649 
     dy_km= 1.100 
-
-    
-    # band1
 
     lat_add = abs(53.3863-(ciudad[0]))
     lon_add = abs((ciudad[1])+137.257)
@@ -31,13 +31,13 @@ def CO2_calc (lat, lon, area, band1):
     #         print(ciudad)
     #         sys.exit(1)
     
-    #print(pos_lat,pos_lon)
+    print(pos_lat,pos_lon)
     #band1[pos_lat,pos_lon]
 
     lon_mitad = int(round(((math.sqrt(ciudad[2])/2)/dx_km)))
     lat_mitad = int(round(((math.sqrt(ciudad[2])/2)/dy_km)))
 
-    #print(lon_mitad,lat_mitad)
+    print(lon_mitad,lat_mitad)
 
     if lon_mitad == 0:        
        c = pos_lon
@@ -59,7 +59,7 @@ def CO2_calc (lat, lon, area, band1):
 
     CO2_sum = np.sum(submatrix)
 
-    #print(CO2_sum)
+    print(CO2_sum)
     return(CO2_sum)
  
 def coordinatesValid(row):
@@ -71,24 +71,36 @@ def coordinatesValid(row):
 
 conn = psycopg2.connect(database="mydb", user="postgres", password="WoodenRumba00", host="192.168.0.120")
 cur = conn.cursor()
-dataset = rasterio.open('Maps/onroad_2017_84.tif')
-band1 = dataset.read(1)
-cur.execute('DELETE FROM public.nasa')
+#cur.execute('DELETE FROM public.nasa')
 
-with open('uscities.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:   
-        if float(row["density"]) > 0 and coordinatesValid(row) :
-            area = float(row["population"])/float(row["density"])     
-            co2=CO2_calc(float(row["lat"]),float(row["lng"]),area,band1)
-            #line_count += 1
-            print(co2)    
-            ratio = co2/float(row["population"])
-            city= row["city"]
-            city = city.replace("'", "''")
-            query=f'INSERT INTO public.nasa VALUES (DEFAULT, \'{city}\', \'{row["lat"]}\', \'{row["lng"]}\',\'{int(float(row["population"]))}\',\'{co2}\',\'{ratio}\' )'
-            cur.execute(query)
+#bucle
+for file in os.listdir("Maps"):
+    dataset = rasterio.open(os.path.join("Maps",file))
+    m = re.search('onroad_(.+?)_84', file)
+    year = int(m.group(1))
+    print(year)
+    band1 = dataset.read(1)
+    #print(band1[(2790-11):(2790+11),(3846-16):(3846+16)])
+
+    with open('uscities.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:   
+            if float(row["density"]) > 0 and coordinatesValid(row) :
+                population = float(row["population"])/(1.037*(2018-year))
+                area = population/float(row["density"])     
+                co2=CO2_calc(float(row["lat"]),float(row["lng"]),area,band1)
+                #line_count += 1
+                #print(co2)    
+                ratio = co2/population
+                # if ratio == 0:
+                #      print(row)
+                city= row["city"]
+                city = city.replace("'", "''")
+                query=f'INSERT INTO public.nasa VALUES (DEFAULT, \'{city}\', \'{row["lat"]}\', \
+                \'{row["lng"]}\',\'{int(population)}\',\'{co2}\',\'{ratio}\',\'{year}\' )'
+                #cur.execute(query)
+                time.sleep(1)
 conn.commit()
 
 # Close communication with the database
